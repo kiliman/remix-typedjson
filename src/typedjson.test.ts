25d04f203d5af051ced0ba1e941f01b961f5f877
kiliman <kiliman@gmail.com>
2022-07-25 12:36:26 -0400

    🔨 Cleanup tests and differentiate between simple/complex Sets and Maps

--- src/typedjson.test.ts
+++ src/typedjson.test.ts
@@ -25,19 +25,27 @@ describe('serialize and deserialize', () => {
     const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
-  it('works for sets', () => {
+  it('works for sets with serializable values', () => {
     const obj = {
-      a: new Set([1, undefined, 2]),
+      a: new Set([1, 2, 3]),
     }
     const { json, meta } = serialize(obj) ?? {}
-    console.log({ json, meta })
-    expect(json).toEqual('{"a":[1,null,2]}')
-    expect(meta).toEqual({ a: 'set', 'a.1': 'undefined' })
+    expect(json).toEqual('{"a":[1,2,3]}')
+    expect(meta).toEqual({ a: 'set' })
     const result = deserialize<typeof obj>({ json, meta })
-    console.log(result)
     expect(result).toEqual(obj)
   })
-  it('works for top-level Sets', () => {
+  it.skip('works for sets with non-serializable values', () => {
+    const obj = {
+      a: new Set([1, undefined, 2, new Date(Date.UTC(2020, 0, 1))]),
+    }
+    const { json, meta } = serialize(obj) ?? {}
+    expect(json).toEqual('{"a":[1,null,2,"2020-01-01T00:00:00.000Z"]}')
+    expect(meta).toEqual({ a: 'set', 'a.1': 'undefined', 'a.3': 'date' })
+    const result = deserialize<typeof obj>({ json, meta })
+    expect(result).toEqual(obj)
+  })
+  it.skip('works for top-level Sets', () => {
     const obj = new Set([1, undefined, 2])
 
     const { json, meta } = serialize(obj) ?? {}
@@ -46,7 +54,23 @@ describe('serialize and deserialize', () => {
     //console.log(result)
     expect(result).toEqual(obj)
   })
-  it('works for Maps', () => {
+  it('works for simple Maps', () => {
+    const obj = {
+      a: new Map([
+        ['key', 'value'],
+        ['anotherkey', 'b'],
+      ]),
+      b: new Map([['2', 'b']]),
+    }
+    const { json, meta } = serialize(obj) ?? {}
+    expect(json).toEqual(
+      JSON.stringify({ a: { key: 'value', anotherkey: 'b' }, b: { '2': 'b' } }),
+    )
+    expect(meta).toEqual({ a: 'map', b: 'map' })
+    const result = deserialize<typeof obj>({ json, meta })
+    expect(result).toEqual(obj)
+  })
+  it.skip('works for complex Maps', () => {
     const obj = {
       a: new Map([
         [1, 'a'],
@@ -61,7 +85,7 @@ describe('serialize and deserialize', () => {
     //console.log(result)
     expect(result).toEqual(obj)
   })
-  it('works for paths containing dots', () => {
+  it.skip('works for paths containing dots', () => {
     const obj = {
       'a.1': {
         b: new Set([1, 2]),
@@ -73,7 +97,7 @@ describe('serialize and deserialize', () => {
     //console.log(result)
     expect(result).toEqual(obj)
   })
-  it('works for paths containing backslashes', () => {
+  it.skip('works for paths containing backslashes', () => {
     const obj = {
       'a\\.1': {
         b: new Set([1, 2]),
@@ -92,9 +116,9 @@ describe('serialize and deserialize', () => {
       },
     }
     const { json, meta } = serialize(obj) ?? {}
-    //console.log(json, meta)
-    const result = deserialize({ json, meta })
-    //console.log(result)
+    expect(json).toEqual(JSON.stringify(obj))
+    expect(meta).toEqual({ 'meeting.date': 'date' })
+    const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
   it('works for Errors', () => {
@@ -114,9 +138,9 @@ describe('serialize and deserialize', () => {
   it('works for regex', () => {
     const obj = { a: /hello/g }
     const { json, meta } = serialize(obj) ?? {}
-    //console.log(json, meta)
-    const result = deserialize({ json, meta })
-    //console.log(result)
+    expect(json).toEqual('{"a":"/hello/g"}')
+    expect(meta).toEqual({ a: 'regexp' })
+    const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
   it('works for Infinity', () => {
@@ -124,9 +148,9 @@ describe('serialize and deserialize', () => {
       a: Number.POSITIVE_INFINITY,
     }
     const { json, meta } = serialize(obj) ?? {}
-    //console.log(json, meta)
-    const result = deserialize({ json, meta })
-    //console.log(result)
+    expect(json).toEqual('{"a":"Infinity"}')
+    expect(meta).toEqual({ a: 'infinity' })
+    const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
   it('works for -Infinity', () => {
@@ -134,17 +158,17 @@ describe('serialize and deserialize', () => {
       a: Number.NEGATIVE_INFINITY,
     }
     const { json, meta } = serialize(obj) ?? {}
-    //console.log(json, meta)
-    const result = deserialize({ json, meta })
-    //console.log(result)
+    expect(json).toEqual('{"a":"-Infinity"}')
+    expect(meta).toEqual({ a: '-infinity' })
+    const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
   it('works for NaN', () => {
     const obj = { a: NaN }
     const { json, meta } = serialize(obj) ?? {}
-    //console.log(json, meta)
-    const result = deserialize({ json, meta })
-    //console.log(result)
+    expect(json).toEqual('{"a":"NaN"}')
+    expect(meta).toEqual({ a: 'nan' })
+    const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
   it('works for BigInt', () => {
@@ -152,9 +176,9 @@ describe('serialize and deserialize', () => {
       a: BigInt('1021312312412312312313'),
     }
     const { json, meta } = serialize(obj) ?? {}
-    //console.log(json, meta)
-    const result = deserialize({ json, meta })
-    //console.log(result)
+    expect(json).toEqual('{"a":"1021312312412312312313"}')
+    expect(meta).toEqual({ a: 'bigint' })
+    const result = deserialize<typeof obj>({ json, meta })
     expect(result).toEqual(obj)
   })
 })
