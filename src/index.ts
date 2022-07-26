a3aa9e6bdb6c1b5ef465625191a8940c8f4a6536
kiliman <kiliman@gmail.com>
2022-07-26 15:39:28 -0400

    🔨 Replace wildcard export with specific items

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-export * from './remix'
-export * from './typedjson'
+export {
+  deserializeRemix,
+  redirect,
+  stringifyRemix,
+  typedjson,
+  useTypedActionData,
+  useTypedFetcher,
+  useTypedLoaderData,
+} from './remix'
+export {
+  applyMeta,
+  deserialize,
+  parse,
+  serialize,
+  stringify,
+} from './typedjson'
+export type { MetaType, TypedJsonResult } from './typedjson'
